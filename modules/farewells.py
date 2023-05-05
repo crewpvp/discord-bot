0572d523a3603126f044dc7d6c0172a2d914ead2
Lotzyprod <trek.ru@yandex.ru>
2023-05-05 23:14:28 +0000

    make it cog

--- modules/farewells.py
+++ modules/farewells.py
@@ -1,17 +1,17 @@
 import discord
+from discord.ext import commands
 import random
-class DiscordFarewell:
+
+class Farewells(commands.Cog):
 	def __init__(self, bot, channels: [int,...], messages: [str,...]):
 		self.bot = bot
 		self.channels = channels
 		self.messages = messages
-		
-		async def member_remove(member: discord.Member):
-			guild = self.bot.guild()
-			message = random.choice(messages).format(member=str(member))
-			for channel in channels:
-				try:
-					await guild.get_channel(channel).send(content=message)
-				except:
-					pass
-		self.member_remove = member_remove
+
+	@commands.Cog.listener()
+	async def on_member_remove(self,member: discord.Member):
+		guild = self.bot.guild()
+		message = random.choice(self.messages).format(member=str(member))
+		for channel in self.channels:
+			if (channel:=guild.get_channel(channel)):
+				await channel.send(content=message)
--- modules/greetings.py
+++ modules/greetings.py
@@ -1,17 +1,17 @@
 import discord
+from discord.ext import commands
 import random
-class DiscordGreeting:
+
+class Greetings(commands.Cog):
 	def __init__(self, bot, channels: [int,...], messages: [str,...]):
 		self.bot = bot
 		self.channels = channels
 		self.messages = messages
-		
-		async def member_join(member: discord.Member):
-			guild = self.bot.guild()
-			message = random.choice(messages).format(member=member.mention)
-			for channel in channels:
-				try:
-					await guild.get_channel(channel).send(content=message)
-				except:
-					pass
-		self.member_join = member_join
+
+	@commands.Cog.listener()
+	async def on_member_join(self,member: discord.Member):
+		guild = self.bot.guild()
+		message = random.choice(self.messages).format(member=member.mention)
+		for channel in self.channels:
+			if (channel:=guild.get_channel(channel)):
+				await channel.send(content=message)
