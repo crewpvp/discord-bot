6e471abb57a43434254f2981706bd7ec2d5c9f24
Lotzyprod <trek.ru@yandex.ru>
2023-05-08 12:30:23 +0000

    remake into cogs

---
+++ modules/manager.py
@@ -0,0 +1,56 @@
+import discord,re
+from discord import app_commands
+from utils import json_to_message, message_to_json
+from discord.ext import commands
+
+class Manager(commands.Cog):
+	def __init__(self,bot):
+		self.bot = bot
+		self.bot.tree.add_command(app_commands.ContextMenu(name='Получить JSON',callback=self.context_message_view))
+
+	messages_group = app_commands.Group(name='messages', description='Менеджер сообщений')
+	
+	@messages_group.command(name='send', description='отправить сообщение из JSON')
+	@app_commands.describe(json='сообщение в формате JSON')
+	async def messages_send(self,interaction: discord.Interaction, json: str):
+		try:
+			content, reference, embeds, components = json_to_message(json)
+			message = await interaction.channel.send(content=content, reference=reference, embeds = embeds, view=components)
+			embed = discord.Embed(description='Сообщение отправлено', color=discord.Colour.green())
+		except:
+			embed = discord.Embed(description='Ошибка в обработке JSON, проверьте правильность вашего сообщения', color=discord.Colour.red())
+		await interaction.response.send_message(embeds=embed,ephemeral=True)
+	
+	@messages_group.command(name='view', description='получить JSON сообщения')
+	@app_commands.rename(id='id_сообщения')
+	async def messages_view(self, interaction: discord.Interaction,id: str):
+		if not re.match('[0-9]*',id) is not None:
+			embed = discord.Embed(description='Неверный ID сообщения', color=discord.Colour.red())
+			await interaction.response.send_message(embed=embed,ephemeral=True)
+			return
+		id = int(id)
+		message = await interaction.channel.fetch_message(id)
+		if not message:
+			embed = discord.Embed(description='Сообщение с данным ID не найдено в канале', color=discord.Colour.red())
+			await interaction.response.send_message(embed=embed,ephemeral=True)
+			return
+		embed = discord.Embed(description = f'```json\n{message_to_json(message)}```',colour = discord.Colour.green())	
+		await interaction.response.send_message(embed=embed,ephemeral=True)
+	
+	@messages_group.command(name='purge', description='очистить сообщения')
+	@app_commands.rename(limit='количество')
+	async def messages_purge(self, interaction: discord.Interaction, limit: int, author: discord.Member = None):
+		await interaction.response.defer(ephemeral=True)
+		limit = abs(limit)
+		async for message in interaction.channel.history(limit=limit):
+			if not author:
+				await message.delete()
+				continue
+			if message.author == author:
+				await message.delete()
+		embed = discord.Embed(description='Сообщения очищены',color=discord.Colour.green())
+		await interaction.followup.send(embed=embed,ephemeral=True)
+
+	async def context_message_view(self, interaction: discord.Interaction, message: discord.Message):
+		await self.message_view.callback(interaction,str(message.id))
+	
