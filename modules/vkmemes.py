a948671a44c7ae7dcceceaca4e7dde090791d98f
Lotzyprod <trek.ru@yandex.ru>
2023-05-09 23:14:58 +0000

    rewritten to use in cogs

--- modules/vkmemes.py
+++ modules/vkmemes.py
@@ -1,62 +1,74 @@
 from vkbottle import Bot, SingleAiohttpClient
 from io import BytesIO
-import discord
-import logging
-import json
-import os
-from loguru import logger
+import discord,logging,json,os
+from discord.ext import commands, tasks
 
-class DiscordVkMemes:
+logging.getLogger("vkbottle").setLevel(logging.ERROR)
+#logger.disable("vkbottle")
+
+class VkMemes(commands.Cog):
 	def __init__(self, bot, token: int, groups:[int,...], channels:[int,...], check_every_seconds: int):
-		logging.getLogger("vkbottle").setLevel(logging.INFO)
-		logger.disable("vkbottle")
+		self.bot = bot
+
 		self.token = token
 		self.vk = Bot(self.token)
 		self.vk.api.http_client = SingleAiohttpClient()
-		self.bot = bot
-		self.groups = [str(group) for group in groups]
+		
 		self.last_posts = {}
-		self.check_every_seconds = check_every_seconds
+		for group in groups:
+			self.last_posts[str(group)] = None
+		self.load_data()
+
 		self.channels = channels
 
-		for group in self.groups:
-			self.last_posts[group] = None
+		self.check_every_seconds = check_every_seconds
+		self.posts_check = tasks.loop(seconds=self.check_every_seconds)(self.on_posts_check)
 		
-		if os.path.isfile("vkmemes.json"):
-			with open('vkmemes.json') as json_file:
-				data = json.load(json_file)
-				for group in data.keys():
-					if group in self.last_posts:
-						self.last_posts[group] = data[group]
+	@commands.Cog.listener()
+	async def on_ready(self):
+		self.posts_check.start()
 
-		async def check(num):
-			if (num % self.check_every_seconds != 0):
-				return
-			guild = self.bot.guild()
-			channels = [guild.get_channel(channel_id) for channel_id in self.channels]
-			for groupid in self.groups:
-				if not self.last_posts[groupid]:
-					self.last_posts[groupid] = (await self.vk.api.wall.get(owner_id=groupid,offset=1,count=1)).items[0].id
+	def cog_unload(self):
+		self.posts_check.cancel()
+
+	async def on_posts_check(self):
+		guild = self.bot.guild()
+		channels = [guild.get_channel(channel_id) for channel_id in self.channels]
+		for group_id in self.last_posts.keys():
+			if not self.last_posts[group_id]:
+				self.last_posts[group_id] = (await self.vk.api.wall.get(owner_id=group_id,offset=1,count=1)).items[0].id
+				continue
+			posts = await self.vk.api.wall.get(owner_id=group_id,offset=1, count=20)
+			for post in reversed(posts.items):
+				if post.id <= self.last_posts[group_id]:
 					continue
-				posts = await self.vk.api.wall.get(owner_id=groupid,offset=1, count=10)
-				for post in reversed(posts.items):
-					if post.id <= self.last_posts[groupid]:
-						continue
-					self.last_posts[groupid] = post.id
-					if not post.attachments:
-						continue
-					if post.attachments[0].photo is None:
-						continue
-					photos = []
-					try:
-						for photo in post.attachments:
-							photos.append(discord.File(BytesIO(await self.download_raw_file(max(photo.photo.sizes, key=lambda x: (x.height, x.width)).url)),filename="image.png"))
-						for channel in channels:
-							await channel.send(files=photos)
-					except:
-						pass
-			with open('vkmemes.json', 'w') as outfile:
-				json.dump(self.last_posts, outfile)
-		self.check = check
+				self.last_posts[group_id] = post.id
+				if not post.attachments:
+					continue
+				if post.attachments[0].photo is None:
+					continue
+				photos = []
+				try:
+					for photo in post.attachments:
+						photos.append(discord.File(BytesIO(await self.download_raw_file(max(photo.photo.sizes, key=lambda x: (x.height, x.width)).url)),filename="image.png"))
+					for channel in channels:
+						await channel.send(files=photos)
+				except:
+					pass
+		self.save_data()
+
 	async def download_raw_file(self,url):
 		return await self.vk.api.http_client.request_content(url)
+
+	def save_data(self):
+		with open('vkmemes.json', 'w') as outfile:
+			json.dump(self.last_posts, outfile)
+
+	def load_data(self):
+		if not os.path.isfile("vkmemes.json"):
+			return
+		with open('vkmemes.json') as json_file:
+			data = json.load(json_file)
+			for group in data.keys():
+				if group in self.last_posts:
+					self.last_posts[group] = data[group]
